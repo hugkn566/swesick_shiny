44e362fdbf43d23a3b3e5429c4e1fd2298be8112
hugkn566 <70522944+hugkn566@users.noreply.github.com>
2020-09-28 12:42:49 +0200

    shiny app

--- swesick_shiny.R
+++ swesick_shiny.R
@@ -2,7 +2,33 @@
 ui <- fluidPage(
   titlePanel("Quarterly ongoing illnesses in Sweden"),
   sidebarLayout(
-    sidebarPanel("test"),
+    sidebarPanel(sliderInput("range", "Year:",
+                             min = 2005, max = (as.numeric(substr(date(), (nchar(date())-3), nchar(date())))-1),
+                             value = c(2005, max), step = 1, sep = ""),
+                 checkboxGroupInput("columns", "Variables to show:",
+                             c("Kvartal" ="kvartal",
+                               "År" = "ar" ,
+                               "Diagnoskapitel text" = "diagnoskapitel_text",
+                               "Andel" = "andel",
+                               "Diagnoskapitel kod" = "diagnoskapitel_kod" ,
+                               "Antal" = "antal", 
+                               "Antal män" = "antal_man",
+                               "Andel kvinnor" = "andel_kvinnor", 
+                               "Antal kvinnor" = "antal_kvinnor", 
+                               "Andel män" = "andel_man"
+                               ), width = '100%',
+                             inline = TRUE,
+                             selected = c("kvartal",
+                                          "ar",
+                                          "diagnoskapitel_text",
+                                          "andel",
+                                          "diagnoskapitel_kod",
+                                          "antal",
+                                          "antal_man",
+                                          "andel_kvinnor",
+                                          "antal_kvinnor",
+                                          "andel_man")
+                             )),
     mainPanel("TEST",
               tags$head(tags$style("tfoot {display: table-header-group;}")),
               fluidRow(
@@ -15,8 +41,12 @@ ui <- fluidPage(
 server <- function(input, output){
   
   dataInput <- reactive({
-    swesick()
+    data <- data
+    data <- data[data$ar >= input$range[1] & data$ar <= input$range[2], input$columns]
     })
+  
+  
+  
   
   output$table <- renderDataTable(dataInput())
 }
