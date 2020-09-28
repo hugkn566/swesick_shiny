711045ec86eaffc2bbe002f206ca276fbbf02e6c
Otto Moen <ottmo242@student.liu.se>
2020-09-28 11:40:51 +0200

    column filter

--- swesick_shiny.R
+++ swesick_shiny.R
@@ -1,5 +1,4 @@
 #app.r
-
 ui <- fluidPage(
   titlePanel("Quarterly ongoing illnesses in Sweden"),
   sidebarLayout(
@@ -8,11 +7,14 @@ ui <- fluidPage(
               fluidRow(
                 column(12,
                        dataTableOutput("table")))
-              )
-  )
+    )
+  ),
+  tags$head(tags$style("tfoot {display: table-header-group;}"))
 )
 
 server <- function(input, output){
+  
+  
   output$table <- renderDataTable(swesick())
 }
 
