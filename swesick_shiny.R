8b9eebd29930b58bde59005484682c5cc052ddc2
Otto Moen <ottmo242@student.liu.se>
2020-09-28 11:05:15 +0200

    app basics

--- swesick_shiny.R
+++ swesick_shiny.R
@@ -1,1 +1,19 @@
 #app.r
+
+ui <- fluidPage(
+  titlePanel("Quarterly ongoing illnesses in Sweden"),
+  sidebarLayout(
+    sidebarPanel("test"),
+    mainPanel("TEST",
+              fluidRow(
+                column(12,
+                       dataTableOutput("table")))
+              )
+  )
+)
+
+server <- function(input, output){
+  output$table <- renderDataTable(swesick())
+}
+
+shinyApp(ui, server)
